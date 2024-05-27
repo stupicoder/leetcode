8ca90ae41726b30b370194bc39247adb65d42ccb
StupidCoder <76776204+stupicoder@users.noreply.github.com>
2024-05-27 14:30:22 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        vector<int> result;
+
+        for(int i = 0; i < nums.size(); ++i)
+        {
+            int& curNum = nums[i];
+            if (curNum > target)
+            {
+                continue;
+            }
+            else if (curNum <= target)
+            {
+                for(int j = i + 1; j < nums.size(); ++j)
+                {
+                    if (curNum + nums[j] == target)
+                    {
+                        result.push_back(i);
+                        result.push_back(j);
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+};
