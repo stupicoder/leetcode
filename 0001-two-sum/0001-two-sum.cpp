2921ef1007827d651e79d571665a9a1eb333f85d
StupidCoder <76776204+stupicoder@users.noreply.github.com>
2024-05-27 14:37:34 +0900

    Time: 48 ms (40.66%), Space: 12.5 MB (99.2%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,10 +4,9 @@ public:
         const size_t num = nums.size();
         for(int i = 0; i < num; ++i)
         {
-            int curNum = nums[i];
             for(int j = i + 1; j < num; ++j)
             {
-                if (curNum + nums[j] == target)
+                if (nums[i] + nums[j] == target)
                 {
                     return { i, j };
                 }
