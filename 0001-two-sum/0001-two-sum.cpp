31ba9da4438e864ce32bbb8b73b823303a1a623f
StupidCoder <76776204+stupicoder@users.noreply.github.com>
2024-05-27 14:30:30 +0900

    Time: 215 ms (7.03%), Space: 12.6 MB (73.84%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -6,19 +6,12 @@ public:
         for(int i = 0; i < nums.size(); ++i)
         {
             int& curNum = nums[i];
-            if (curNum > target)
+            for(int j = i + 1; j < nums.size(); ++j)
             {
-                continue;
-            }
-            else if (curNum <= target)
-            {
-                for(int j = i + 1; j < nums.size(); ++j)
+                if (curNum + nums[j] == target)
                 {
-                    if (curNum + nums[j] == target)
-                    {
-                        result.push_back(i);
-                        result.push_back(j);
-                    }
+                    result.push_back(i);
+                    result.push_back(j);
                 }
             }
         }
