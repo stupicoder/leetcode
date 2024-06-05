29dd663a9dab04b7ddcfc71ac0703368e3d90452
StupidCoder <76776204+stupicoder@users.noreply.github.com>
2024-06-05 15:03:32 +0900

    Time: 20 ms (63.72%), Space: 76 MB (84.41%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -19,17 +19,11 @@ public:
         while(l1 != nullptr || l2 != nullptr || sum != 0)
         {
             ListNode* NewNode = new ListNode();
-            if (l1 != nullptr)
-            {
-                sum += l1->val;
-                l1 = l1->next;
-            }
-            if (l2 != nullptr)
-            {
-                sum += l2->val;
-                l2 = l2->next;
-            }
-
+            const int r1 = (l1 == nullptr) ? 0 : l1->val;
+            const int r2 = (l2 == nullptr) ? 0 : l2->val;
+            if (l1 != nullptr) { l1 = l1->next; }
+            if (l2 != nullptr) { l2 = l2->next; }
+            sum = sum + r1 + r2;
             NewNode->val = sum % 10;
             sum /= 10;
             if (result == nullptr)
