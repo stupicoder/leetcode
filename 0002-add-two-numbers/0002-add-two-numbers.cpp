d922ed6ec58ef8b4b32238cbb0d7a36a5a68a3d5
StupidCoder <76776204+stupicoder@users.noreply.github.com>
2024-06-05 15:08:18 +0900

    Time: 26 ms (31.26%), Space: 76 MB (84.41%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.cpp
+++ 0002-add-two-numbers/0002-add-two-numbers.cpp
@@ -25,7 +25,7 @@ public:
             if (l2 != nullptr) { l2 = l2->next; }
             sum = sum + r1 + r2;
             NewNode->val = (sum > 9) ? sum - 10 : sum;
-            sum /= 10;
+            sum = (sum > 9);
             if (result == nullptr)
             {
                 result = NewNode;
